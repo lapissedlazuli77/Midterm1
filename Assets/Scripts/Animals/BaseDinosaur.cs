d1004603f94661f8e442389285a401e510475d25
William Suwandi <ws2315@nyu.edu>
2023-03-06 12:18:18 -0500

    MORE MOVEMENT WHEEE

--- Assets/Scripts/Animals/BaseDinosaur.cs
+++ Assets/Scripts/Animals/BaseDinosaur.cs
@@ -9,10 +9,13 @@ public class BaseDinosaur : MonoBehaviour
     NavMeshAgent nav;
 
     bool hunting = false;
+    bool moving = true;
 
     float timer = 0;
     float timertime;
     float hunttime;
+    float idletime;
+    int idlethresh;
 
     public float posx;
     public float posz;
@@ -22,6 +25,9 @@ public class BaseDinosaur : MonoBehaviour
     AudioSource thissound;
     public AudioClip walksound;
     public AudioClip runsound;
+    public AudioClip idlesound;
+    public AudioClip huntsound;
+    public AudioClip deathsound;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +37,7 @@ public class BaseDinosaur : MonoBehaviour
 
         timertime = Random.Range(4f, 10f);
         hunttime = 10f;
+        idletime = Random.Range(1f, 5f);
 
         posx = Random.Range(-100f, 100f);
         posz = Random.Range(-100f, 100f);
@@ -43,16 +50,19 @@ public class BaseDinosaur : MonoBehaviour
         if (hunting)
         {
             Chase();
-        }
-        else
+        } else if (!hunting && moving)
         {
             Wander();
+        } else
+        {
+            Idle();
         }
         timer += Time.deltaTime;
     }
-    virtual void Chase()
+    protected virtual void Chase()
     {
         thissound.clip = runsound;
+        thissound.loop = true;
         nav.destination = player.position;
         if (timer >= hunttime)
         {
@@ -64,18 +74,44 @@ public class BaseDinosaur : MonoBehaviour
             destination.Set(posx, 0, posz);
         }
     }
-    virtual void Wander()
+    protected virtual void Wander()
     {
         thissound.clip = walksound;
+        thissound.loop = true;
         nav.destination = destination;
         if (timer >= timertime)
+        {
+            int behavchooser = Random.Range(1, 4);
+            if (behavchooser >= idlethresh)
+            {
+                timertime = Random.Range(4f, 10f);
+                timer = 0;
+                moving = false;
+            } else
+            {
+                posx = Random.Range(-100f, 100f);
+                posz = Random.Range(-100f, 100f);
+                destination.Set(posx, 0, posz);
+
+                timertime = Random.Range(4f, 10f);
+                timer = 0;
+            }
+        }
+    }
+    protected virtual void Idle()
+    {
+        thissound.clip = idlesound;
+        thissound.loop = false;
+        nav.destination = transform.position;
+        if (timer >= idletime)
         {
             posx = Random.Range(-100f, 100f);
             posz = Random.Range(-100f, 100f);
             destination.Set(posx, 0, posz);
 
-            timertime = Random.Range(4f, 10f);
+            idletime = Random.Range(1f, 5f);
             timer = 0;
+            moving = true;
         }
     }
 }
--- Assets/Scripts/Animals/Carnotaurus.cs
+++ Assets/Scripts/Animals/Carnotaurus.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Carnotaurus : MonoBehaviour
+public class Carnotaurus : BaseDinosaur
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
